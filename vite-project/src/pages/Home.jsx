41e14ecc164d4d2bef7b81ecb0988cc564c16f57
joseph-cef <josephdev1962@gmail.com>
2025-07-09 15:49:26 +0100

    Add StarBackground component for animated star and meteor effects

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import {ThemeToggle} from '../Components/ThemeToggle';
+import {ThemeToggle} from '../Components/ThemeToggle'; 
+import {StarBackground} from '../Components/StarBackground';
 const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         {/* Theme Toggle */}
     <ThemeToggle />
         {/* Bg Effects */}
-
+    <StarBackground />
         {/* Navbar */}
 
         {/* Main Content */}
