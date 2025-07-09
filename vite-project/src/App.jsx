010f2c4f5c906ec976cd3eede43de7585c2aafea
joseph-cef <josephdev1962@gmail.com>
2025-07-09 19:30:12 +0100

    feat: set basename for BrowserRouter in App component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,11 +7,12 @@ function App() {
   return (
     <>
       <Toaster />
-      <BrowserRouter>
+      <BrowserRouter basename="/Portfolio-cefyou">
         <Routes>
-          <Route index element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
+
       </BrowserRouter>
     </>
   );
