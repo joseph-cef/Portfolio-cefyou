48e0518dc08390df68f625e65cf78b464d4d0c47
joseph-cef <josephdev1962@gmail.com>
2025-07-09 19:23:25 +0100

    feat: update package.json and vite.config.js to include gh-pages and set base path

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/Portfolio-cefyou/', // مهم جداً
+
   plugins: [
            react(), 
            tailwindcss()],
