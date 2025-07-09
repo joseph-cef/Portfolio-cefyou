4646f1da5aa31504b7bdd99c16c4949dbef62860
joseph-cef <josephdev1962@gmail.com>
2025-07-09 15:18:02 +0100

    Add utility function for conditional class names and update ThemeToggle component styling

--- vite-project/src/Components/ThemeToggle.jsx
+++ vite-project/src/Components/ThemeToggle.jsx
@@ -1,5 +1,6 @@
 import {Sun, Moon} from 'lucide-react';
 import   { useEffect, useState } from 'react';
+import { cn } from '../lib/utils';
 
 export const ThemeToggle = () => {
 const [isDarkMode, setisDarkMode] = useState(false);
@@ -26,6 +27,10 @@ const toggleTheme = () => {
      }
 }
     return(
-        <button onClick={toggleTheme}>{isDarkMode ? <Sun  className='h-6 w-6 text-yellow-300'/>: <Moon className='h-6 w-6 text-blue-300'/>}</button>
+        <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full bg-gray-200 dark:bg-gray-800 transition-colors duration-300 hover:bg-gray-300 dark:hover:bg-gray-700")}>
+            {isDarkMode ? 
+            <Sun  className='h-6 w-6 text-yellow-300'/>: 
+            <Moon className='h-6 w-6 text-blue-300'/>
+            }</button>
     )
 };
