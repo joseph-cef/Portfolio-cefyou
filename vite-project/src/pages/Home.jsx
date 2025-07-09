7768f17e7349c7fa37a59c892587adec570eec5c
joseph-cef <josephdev1962@gmail.com>
2025-07-09 14:51:47 +0100

    first commit

---
+++ vite-project/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+ 
+const Home = () => {
+  return (
+    <div className="min-h-screen ">
+        {/* Theme Toggle */}
+
+        {/* Bg Effects */}
+
+        {/* Navbar */}
+
+        {/* Main Content */}
+    </div>
+  )
+}
+
+export default Home
